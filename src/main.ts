28c404d0cc71fb88da2c502b65e5a43de1810747
tluwaniMS <tluwani2020@gmail.com>
2025-06-13 23:02:27 +0200

    adding-http-middleware

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
-
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  const format =
+    ':method :url :status :res[content-length] - :response-time ms';
+
+  // HTTP request logging middleware
+  const morganMiddleware = morgan(format, {
+    stream: {
+      write: (message: string) => {
+        Logger.log(message.trim(), 'HTTP');
+        return true;
+      },
+    },
+  });
+
+  app.use(morganMiddleware);
 
   const config = new DocumentBuilder()
     .setTitle('Basic Nest and MongoDB API')
